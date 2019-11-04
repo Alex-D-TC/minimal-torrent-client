c8eb3f43d9cddc56134b8b4431e0c00bd18393be
Alexandru Duma <badco25@gmail.com>
2019-11-04 23:16:10 +0200

    Add custom errors to the thread pool

--- util/thread_pool.go
+++ util/thread_pool.go
@@ -1,5 +1,21 @@
 package util
 
+// ThreadPoolClosingError is a custom error class
+type ThreadPoolClosingError struct{}
+
+// Used to implement the error interface
+func (err *ThreadPoolClosingError) Error() string {
+	return "Thread pool is closing"
+}
+
+// TaskSubmissionFailedError is a custom error class
+type TaskSubmissionFailedError struct{}
+
+// Used to implement the error interface
+func (err *TaskSubmissionFailedError) Error() string {
+	return "Task submission failed"
+}
+
 const queueSize = 1024
 
 // ThreadPool is a struct which holds the state of a thread pool
@@ -28,17 +44,17 @@ func MakeThreadPool(workerCount int) ThreadPool {
 
 // Submit submits a task to the thread pool. If all workers are busy and the queue is full, the operation returns false.
 // The operation also returns false if the thread pool is closing
-func (pool *ThreadPool) Submit(op func()) bool {
+func (pool *ThreadPool) Submit(op func()) error {
 	if !pool.isClosed {
 		select {
 		case pool.taskChannel <- op:
-			return true
+			return nil
 		default:
 			// Message submission failed
-			return false
+			return &TaskSubmissionFailedError{}
 		}
 	}
-	return false
+	return &ThreadPoolClosingError{}
 }
 
 // Terminate signals the termination of a thread pool. The work queue is cleared and all workers finish their tasks, then quit.
